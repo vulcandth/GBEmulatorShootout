67e576bdc2eeb6d800e2ec0f3bdc226bb5b7f423
vulcandth <vulcandth@gmail.com>
2025-06-19 20:37:41 -0400

    Add vibeEmu emulator

---
+++ emulators/vibeemu.py
@@ -0,0 +1,26 @@
+from util import *
+from emulator import Emulator
+from test import *
+import os
+import subprocess
+
+
+class VibeEmu(Emulator):
+    def __init__(self):
+        super().__init__("vibeEmu", "https://github.com/vulcandth/vibeEmu", startup_time=1.0)
+        self.title_check = lambda title: "vibeEmu" in title
+
+    def setup(self):
+        download("https://codeload.github.com/vulcandth/vibeEmu/zip/main", "downloads/vibeemu.zip")
+        extract("downloads/vibeemu.zip", "emu/vibeemu")
+        self.path = os.path.join("emu", "vibeemu", os.listdir("emu/vibeemu")[0])
+        subprocess.Popen(["cargo", "build", "--release"], cwd=self.path).wait()
+        self.exe = os.path.join(self.path, "target", "release", "vibeEmu.exe")
+        setDPIScaling(self.exe)
+        setupMesa(os.path.dirname(self.exe))
+
+    def startProcess(self, rom, *, model, required_features):
+        mode = {DMG: "--dmg", CGB: "--cgb"}.get(model)
+        if mode is None:
+            return None
+        return subprocess.Popen([self.exe, mode, os.path.abspath(rom)], cwd=self.path)
