e3bac6dd248c7058aafe49890b2f24c237fa4e16
vulcandth <vulcandth@gmail.com>
2025-06-20 08:24:41 -0400

    Normalize vibeEmu output

--- emulators/vibeemu.py
+++ emulators/vibeemu.py
@@ -3,6 +3,8 @@ from emulator import Emulator
 from test import *
 import os
 import subprocess
+import PIL.Image
+import PIL.ImageOps
 
 
 class VibeEmu(Emulator):
@@ -24,3 +26,12 @@ class VibeEmu(Emulator):
         if mode is None:
             return None
         return subprocess.Popen([self.exe, mode, os.path.abspath(rom)], cwd=self.path)
+
+    def getScreenshot(self):
+        screenshot = super().getScreenshot()
+        if screenshot is None:
+            return None
+        screenshot = screenshot.resize((160, 144), PIL.Image.NEAREST)
+        screenshot = screenshot.convert(mode="L", dither=PIL.Image.NONE)
+        screenshot = PIL.ImageOps.autocontrast(screenshot)
+        return screenshot
